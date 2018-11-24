804591e8c6ab70221c36638b69940f93279c97e6
Ryan Brant <ryanjbrant@gmail.com>
2018-11-24 12:52:50 -0800

    Initial Commit

---
+++ montage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  montage
+//
+//  Created by Ryan Brant on 11/24/18.
+//  Copyright © 2018 Ryan Brant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
